40d2e4d4dd6676a63fd6737adafd600e021a433b
stvfu <stvfu.fu@gmail.com>
2024-04-12 02:56:19 +0000

    [build_tree] Add crypto api wrapper
    Add crypto api wrapper
    
    stvfu

---
+++ build_tree/rot_wrapper/rot_wrapper.c
@@ -0,0 +1,183 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <sec_api.h>
+#include <rot_wrapper.h>
+
+#define ROT_TRACE_IN_OUT_LOG_ENABLE
+
+#define TRACE_IN  printf("[%s][%d] IN\n", __func__,__LINE__);
+#define TRACE_OUT printf("[%s][%d] OUT\n", __func__,__LINE__);
+
+// Software implementation
+// random
+rot_error_t rot_trng_generate_random_buffer(uint32_t * datas, uint32_t unmber_of_words)
+{
+    TRACE_IN
+    // [TODO]
+    TRACE_OUT
+    return 0;
+}
+
+// SHA
+rot_error_t rot_hash_sha1(uint32_t * msg, uint32_t* sha_out, uint32_t msg_len)
+{
+    TRACE_IN
+    // [TODO]
+    TRACE_OUT
+    return 0;
+}
+
+rot_error_t rot_hash_sha224(uint32_t * msg, uint32_t* sha_out, uint32_t msg_len)
+{
+    TRACE_IN
+    // [TODO]
+    TRACE_OUT
+    return 0;
+}
+
+rot_error_t rot_hash_sha256(uint32_t * msg, uint32_t* sha_out, uint32_t msg_len)
+{
+    TRACE_IN
+    // [TODO]
+    TRACE_OUT
+    return 0;
+}
+
+rot_error_t rot_hash_sha384(uint32_t * msg, uint32_t* sha_out, uint32_t msg_len)
+{
+    TRACE_IN
+    // [TODO]
+    TRACE_OUT
+    return 0;
+}
+
+rot_error_t rot_hash_sha512(uint32_t * msg, uint32_t* sha_out, uint32_t msg_len)
+{
+    TRACE_IN
+    // [TODO]
+    TRACE_OUT
+    return 0;
+}
+
+// AES
+rot_error_t rot_bc_aes_ecb_enc(uint8_t key_size_in_bytes,
+                               uint32_t *add_key,
+                               uint32_t *add_src,
+                               uint32_t *add_dest,
+                               uint32_t tr_size_in_bytes)
+{
+    TRACE_IN
+    // [TODO]
+    TRACE_OUT
+    return 0;
+}
+
+rot_error_t rot_bc_aes_ecb_dec(uint8_t key_size_in_bytes,
+                               uint32_t *add_key,
+                               uint32_t *add_src,
+                               uint32_t *add_dest,
+                               uint32_t tr_size_in_bytes)
+{
+    TRACE_IN
+    // [TODO]
+    TRACE_OUT
+    return 0;
+}
+
+
+rot_error_t rot_bc_aes_cbc_enc(uint8_t key_size_in_bytes,
+                               uint32_t *add_key,
+                               uint32_t *add_iv,
+                               uint32_t *add_src,
+                               uint32_t *add_dest,
+                               uint32_t tr_size_in_bytes)
+{
+    TRACE_IN
+    // [TODO]
+    TRACE_OUT
+    return 0;
+}
+
+rot_error_t rot_bc_aes_cbc_dec(uint8_t key_size_in_bytes,
+                               uint32_t *add_key,
+                               uint32_t *add_iv,
+                               uint32_t *add_src,
+                               uint32_t *add_dest,
+                               uint32_t tr_size_in_bytes)
+{
+    printf("[TODO]\n");
+    return 0;
+}
+
+rot_error_t rot_bc_aes_ctr_enc(uint8_t key_size_in_bytes,
+                               uint32_t *add_key,
+                               uint32_t *add_iv,
+                               uint32_t *add_src,
+                               uint32_t *add_dest,
+                               uint32_t tr_size_in_bytes)
+{
+    TRACE_IN
+    // [TODO]
+    TRACE_OUT
+    return 0;
+}
+
+rot_error_t rot_bc_aes_ctr_dec(uint8_t key_size_in_bytes,
+                               uint32_t *add_key,
+                               uint32_t *add_iv,
+                               uint32_t *add_src,
+                               uint32_t *add_dest,
+                               uint32_t tr_size_in_bytes)
+{
+    TRACE_IN
+    // [TODO]
+    TRACE_OUT
+    return 0;
+}
+
+rot_error_t rot_rsa_generate_key(uint32_t* const n,
+                                 uint32_t* const p,
+                                 uint32_t* const q,
+                                 uint32_t* const dP,
+                                 uint32_t* const dQ,
+                                 uint32_t* const qInv,
+                                 uint32_t size_n_in_words)
+{
+    TRACE_IN
+    // [TODO]
+    TRACE_OUT
+    return 0;
+}
+
+rot_error_t rot_rsa_verify(uint32_t* n,
+                           uint32_t* exp,
+                           uint16_t  size_n_in_words,
+                           uint16_t  size_e_in_words,
+                           uint32_t  hash_type,
+                           const uint32_t  *add_message,
+                           uint32_t message_size_in_words,
+                           const uint32_t  *add_signature,
+                           uint32_t signature_size_in_words)
+{
+    TRACE_IN
+    // [TODO]
+    TRACE_OUT
+    return 0;
+}
+
+rot_error_t rot_rsa_sign(uint32_t* n,
+                         uint32_t* exp,
+                         uint16_t  size_n_in_words,
+                         uint16_t  size_e_in_words,
+                         uint32_t  hash_type,
+                         const uint32_t  *add_message,
+                         uint32_t message_size_in_words,
+                         const uint32_t  *add_signature,
+                         uint32_t signature_size_in_words)
+{
+    TRACE_IN
+    // [TODO]
+    TRACE_OUT
+    return 0;
+}
+
---
+++ build_tree/rot_wrapper/rot_wrapper.h
@@ -0,0 +1,88 @@
+// Data Types
+typedef uint32_t rot_error_t;
+
+// random
+rot_error_t rot_trng_generate_random_buffer(uint32_t * datas, uint32_t unmber_of_words);
+
+// sha
+rot_error_t rot_hash_sha1(uint32_t * msg, uint32_t* sha_out, uint32_t msg_len);
+rot_error_t rot_hash_sha224(uint32_t * msg, uint32_t* sha_out, uint32_t msg_len);
+rot_error_t rot_hash_sha256(uint32_t * msg, uint32_t* sha_out, uint32_t msg_len);
+rot_error_t rot_hash_sha384(uint32_t * msg, uint32_t* sha_out, uint32_t msg_len);
+rot_error_t rot_hash_sha512(uint32_t * msg, uint32_t* sha_out, uint32_t msg_len);
+
+// AES
+rot_error_t rot_bc_aes_ecb_enc(uint8_t key_size_in_bytes,
+                               uint32_t *add_key,
+                               uint32_t *add_src,
+                               uint32_t *add_dest,
+                               uint32_t tr_size_in_bytes);
+
+rot_error_t rot_bc_aes_ecb_dec(uint8_t key_size_in_bytes,
+                               uint32_t *add_key,
+                               uint32_t *add_src,
+                               uint32_t *add_dest,
+                               uint32_t tr_size_in_bytes);
+
+rot_error_t rot_bc_aes_cbc_enc(uint8_t key_size_in_bytes,
+                               uint32_t *add_key,
+                               uint32_t *add_iv,
+                               uint32_t *add_src,
+                               uint32_t *add_dest,
+                               uint32_t tr_size_in_bytes);
+
+rot_error_t rot_bc_aes_cbc_dec(uint8_t key_size_in_bytes,
+                               uint32_t *add_key,
+                               uint32_t *add_iv,
+                               uint32_t *add_src,
+                               uint32_t *add_dest,
+                               uint32_t tr_size_in_bytes);
+
+rot_error_t rot_bc_aes_ctr_enc(uint8_t key_size_in_bytes,
+                               uint32_t *add_key,
+                               uint32_t *add_iv,
+                               uint32_t *add_src,
+                               uint32_t *add_dest,
+                               uint32_t tr_size_in_bytes);
+
+rot_error_t rot_bc_aes_ctr_dec(uint8_t key_size_in_bytes,
+                               uint32_t *add_key,
+                               uint32_t *add_iv,
+                               uint32_t *add_src,
+                               uint32_t *add_dest,
+                               uint32_t tr_size_in_bytes);
+
+rot_error_t rot_bc_aes_cbc(uint8_t key_size_in_bytes,
+                           uint32_t *add_key,
+                           uint32_t *add_src,
+                           uint32_t *add_dest,
+                           uint32_t tr_size_in_bytes);
+
+// RSA
+rot_error_t rot_rsa_generate_key(uint32_t* const n,
+                                 uint32_t* const p,
+                                 uint32_t* const q,
+                                 uint32_t* const dP,
+                                 uint32_t* const dQ,
+                                 uint32_t* const qInv,
+                                 uint32_t size_n_in_words);
+
+rot_error_t rot_rsa_verify(uint32_t* n,
+                           uint32_t* exp,
+                           uint16_t  size_n_in_words,
+                           uint16_t  size_e_in_words,
+                           uint32_t  hash_type,
+                           const uint32_t  *add_message,
+                           uint32_t message_size_in_words,
+                           const uint32_t  *add_signature,
+                           uint32_t signature_size_in_words);
+
+rot_error_t rot_rsa_sign(uint32_t* n,
+                         uint32_t* exp,
+                         uint16_t  size_n_in_words,
+                         uint16_t  size_e_in_words,
+                         uint32_t  hash_type,
+                         const uint32_t  *add_message,
+                         uint32_t message_size_in_words,
+                         const uint32_t  *add_signature,
+                         uint32_t signature_size_in_words);
